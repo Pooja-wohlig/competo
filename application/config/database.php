ac86576b053e79a3f626b4b67e245162044157bc
Chintan <chintan@wohlig.com>
2015-10-23 19:37:57 +0530

    change in database

--- application/config/database.php
+++ application/config/database.php
@@ -49,29 +49,6 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-
-$myserver=$_SERVER['SERVER_NAME'];
-
-if($myserver!="localhost")
-{
-    $db['default']['hostname'] = 'localhost';
-    $db['default']['username'] = 'wohligco_avinash';
-    $db['default']['password'] = 'avi';
-    $db['default']['database'] = 'avi';
-    $db['default']['dbdriver'] = 'mysql';
-    $db['default']['dbprefix'] = '';
-    $db['default']['pconnect'] = TRUE;
-    $db['default']['db_debug'] = TRUE;
-    $db['default']['cache_on'] = FALSE;
-    $db['default']['cachedir'] = '';
-    $db['default']['char_set'] = 'utf8';
-    $db['default']['dbcollat'] = 'utf8_general_ci';
-    $db['default']['swap_pre'] = '';
-    $db['default']['autoinit'] = TRUE;
-    $db['default']['stricton'] = FALSE;
-}
-else
-{
     $db['default']['hostname'] = 'localhost';
     $db['default']['username'] = 'root';
     $db['default']['password'] = '';
@@ -87,25 +64,5 @@ else
     $db['default']['swap_pre'] = '';
     $db['default']['autoinit'] = TRUE;
     $db['default']['stricton'] = FALSE;
-}
-
-//$db['default']['hostname'] = getconfig("hostname");
-//$db['default']['username'] = getconfig("username");
-//$db['default']['password'] = getconfig("password");
-//$db['default']['database'] = getconfig("database");
-//$db['default']['dbdriver'] = 'mysql';
-//$db['default']['dbprefix'] = '';
-//$db['default']['pconnect'] = TRUE;
-//$db['default']['db_debug'] = TRUE;
-//$db['default']['cache_on'] = FALSE;
-//$db['default']['cachedir'] = '';
-//$db['default']['char_set'] = 'utf8';
-//$db['default']['dbcollat'] = 'utf8_general_ci';
-//$db['default']['swap_pre'] = '';
-//$db['default']['autoinit'] = TRUE;
-//$db['default']['stricton'] = FALSE;
-
-
-
 /* End of file database.php */
 /* Location: ./application/config/database.php */

81a6fecd33cd3782d4605c814d57cda5eb5647ba
Chintan <chintan@wohlig.com>
2015-10-23 20:57:04 +0530

    git changes

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -1,162 +1,162 @@
-<?php if ( ! defined("BASEPATH")) exit("No direct script access allowed");
-class Json extends CI_Controller 
-{function getallcompetition()
-{
-$elements=array();
-$elements[0]=new stdClass();
-$elements[0]->field="`competo_competition`.`id`";
-$elements[0]->sort="1";
-$elements[0]->header="ID";
-$elements[0]->alias="id";
+<?php
 
-$elements=array();
-$elements[1]=new stdClass();
-$elements[1]->field="`competo_competition`.`status`";
-$elements[1]->sort="1";
-$elements[1]->header="Status";
-$elements[1]->alias="status";
-
-$elements=array();
-$elements[2]=new stdClass();
-$elements[2]->field="`competo_competition`.`name`";
-$elements[2]->sort="1";
-$elements[2]->header="Name";
-$elements[2]->alias="name";
-
-$elements=array();
-$elements[3]=new stdClass();
-$elements[3]->field="`competo_competition`.`timestamp`";
-$elements[3]->sort="1";
-$elements[3]->header="Timestamp";
-$elements[3]->alias="timestamp";
-
-$elements=array();
-$elements[4]=new stdClass();
-$elements[4]->field="`competo_competition`.`date`";
-$elements[4]->sort="1";
-$elements[4]->header="Date";
-$elements[4]->alias="date";
-
-$search=$this->input->get_post("search");
-$pageno=$this->input->get_post("pageno");
-$orderby=$this->input->get_post("orderby");
-$orderorder=$this->input->get_post("orderorder");
-$maxrow=$this->input->get_post("maxrow");
-if($maxrow=="")
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+class Json extends CI_Controller
 {
-}
-if($orderby=="")
-{
-$orderby="id";
-$orderorder="ASC";
-}
-$data["message"]=$this->chintantable->query($pageno,$maxrow,$orderby,$orderorder,$search,$elements,"FROM `competo_competition`");
-$this->load->view("json",$data);
-}
-public function getsinglecompetition()
-{
-$id=$this->input->get_post("id");
-$data["message"]=$this->competition_model->getsinglecompetition($id);
-$this->load->view("json",$data);
-}
-function getallcompetitionparticipant()
-{
-$elements=array();
-$elements[0]=new stdClass();
-$elements[0]->field="`competo_competitionparticipant`.`id`";
-$elements[0]->sort="1";
-$elements[0]->header="ID";
-$elements[0]->alias="id";
+    public function getallcompetition()
+    {
+        $elements = array();
+        $elements[0] = new stdClass();
+        $elements[0]->field = '`competo_competition`.`id`';
+        $elements[0]->sort = '1';
+        $elements[0]->header = 'ID';
+        $elements[0]->alias = 'id';
 
-$elements=array();
-$elements[1]=new stdClass();
-$elements[1]->field="`competo_competitionparticipant`.`name`";
-$elements[1]->sort="1";
-$elements[1]->header="Name";
-$elements[1]->alias="name";
+        $elements = array();
+        $elements[1] = new stdClass();
+        $elements[1]->field = '`competo_competition`.`status`';
+        $elements[1]->sort = '1';
+        $elements[1]->header = 'Status';
+        $elements[1]->alias = 'status';
 
-$search=$this->input->get_post("search");
-$pageno=$this->input->get_post("pageno");
-$orderby=$this->input->get_post("orderby");
-$orderorder=$this->input->get_post("orderorder");
-$maxrow=$this->input->get_post("maxrow");
-if($maxrow=="")
-{
-}
-if($orderby=="")
-{
-$orderby="id";
-$orderorder="ASC";
-}
-$data["message"]=$this->chintantable->query($pageno,$maxrow,$orderby,$orderorder,$search,$elements,"FROM `competo_competitionparticipant`");
-$this->load->view("json",$data);
-}
-public function getsinglecompetitionparticipant()
-{
-$id=$this->input->get_post("id");
-$data["message"]=$this->competitionparticipant_model->getsinglecompetitionparticipant($id);
-$this->load->view("json",$data);
-}
-function getallcompetitionscore()
-{
-$elements=array();
-$elements[0]=new stdClass();
-$elements[0]->field="`competo_competitionscore`.`id`";
-$elements[0]->sort="1";
-$elements[0]->header="ID";
-$elements[0]->alias="id";
+        $elements = array();
+        $elements[2] = new stdClass();
+        $elements[2]->field = '`competo_competition`.`name`';
+        $elements[2]->sort = '1';
+        $elements[2]->header = 'Name';
+        $elements[2]->alias = 'name';
 
-$elements=array();
-$elements[1]=new stdClass();
-$elements[1]->field="`competo_competitionscore`.`user`";
-$elements[1]->sort="1";
-$elements[1]->header="User";
-$elements[1]->alias="user";
+        $elements = array();
+        $elements[3] = new stdClass();
+        $elements[3]->field = '`competo_competition`.`timestamp`';
+        $elements[3]->sort = '1';
+        $elements[3]->header = 'Timestamp';
+        $elements[3]->alias = 'timestamp';
 
-$elements=array();
-$elements[2]=new stdClass();
-$elements[2]->field="`competo_competitionscore`.`competitionparticipant`";
-$elements[2]->sort="1";
-$elements[2]->header="Competition Participant";
-$elements[2]->alias="competitionparticipant";
+        $elements = array();
+        $elements[4] = new stdClass();
+        $elements[4]->field = '`competo_competition`.`date`';
+        $elements[4]->sort = '1';
+        $elements[4]->header = 'Date';
+        $elements[4]->alias = 'date';
 
-$elements=array();
-$elements[3]=new stdClass();
-$elements[3]->field="`competo_competitionscore`.`score`";
-$elements[3]->sort="1";
-$elements[3]->header="Score";
-$elements[3]->alias="score";
+        $search = $this->input->get_post('search');
+        $pageno = $this->input->get_post('pageno');
+        $orderby = $this->input->get_post('orderby');
+        $orderorder = $this->input->get_post('orderorder');
+        $maxrow = $this->input->get_post('maxrow');
+        if ($maxrow == '') {
+        }
+        if ($orderby == '') {
+            $orderby = 'id';
+            $orderorder = 'DESC';
+        }
+        $data['message'] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, 'FROM `competo_competition`');
+        $this->load->view('json', $data);
+    }
+    public function getsinglecompetition()
+    {
+        $id = $this->input->get_post('id');
+        $data['message'] = $this->competition_model->getsinglecompetition($id);
+        $this->load->view('json', $data);
+    }
+    public function getallcompetitionparticipant()
+    {
+        $elements = array();
+        $elements[0] = new stdClass();
+        $elements[0]->field = '`competo_competitionparticipant`.`id`';
+        $elements[0]->sort = '1';
+        $elements[0]->header = 'ID';
+        $elements[0]->alias = 'id';
 
-$elements=array();
-$elements[4]=new stdClass();
-$elements[4]->field="`competo_competitionscore`.`comments`";
-$elements[4]->sort="1";
-$elements[4]->header="comments";
-$elements[4]->alias="comments";
+        $elements = array();
+        $elements[1] = new stdClass();
+        $elements[1]->field = '`competo_competitionparticipant`.`name`';
+        $elements[1]->sort = '1';
+        $elements[1]->header = 'Name';
+        $elements[1]->alias = 'name';
 
-$search=$this->input->get_post("search");
-$pageno=$this->input->get_post("pageno");
-$orderby=$this->input->get_post("orderby");
-$orderorder=$this->input->get_post("orderorder");
-$maxrow=$this->input->get_post("maxrow");
-if($maxrow=="")
-{
-}
-if($orderby=="")
-{
-$orderby="id";
-$orderorder="ASC";
-}
-$data["message"]=$this->chintantable->query($pageno,$maxrow,$orderby,$orderorder,$search,$elements,"FROM `competo_competitionscore`");
-$this->load->view("json",$data);
-}
-public function getsinglecompetitionscore()
-{
-$id=$this->input->get_post("id");
-$data["message"]=$this->competitionscore_model->getsinglecompetitionscore($id);
-$this->load->view("json",$data);
-}
-  public function signIn()
+        $search = $this->input->get_post('search');
+        $pageno = $this->input->get_post('pageno');
+        $orderby = $this->input->get_post('orderby');
+        $orderorder = $this->input->get_post('orderorder');
+        $maxrow = $this->input->get_post('maxrow');
+        if ($maxrow == '') {
+        }
+        if ($orderby == '') {
+            $orderby = 'id';
+            $orderorder = 'ASC';
+        }
+        $data['message'] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, 'FROM `competo_competitionparticipant`');
+        $this->load->view('json', $data);
+    }
+    public function getsinglecompetitionparticipant()
+    {
+        $id = $this->input->get_post('id');
+        $data['message'] = $this->competitionparticipant_model->getsinglecompetitionparticipant($id);
+        $this->load->view('json', $data);
+    }
+    public function getallcompetitionscore()
+    {
+        $elements = array();
+        $elements[0] = new stdClass();
+        $elements[0]->field = '`competo_competitionscore`.`id`';
+        $elements[0]->sort = '1';
+        $elements[0]->header = 'ID';
+        $elements[0]->alias = 'id';
+
+        $elements = array();
+        $elements[1] = new stdClass();
+        $elements[1]->field = '`competo_competitionscore`.`user`';
+        $elements[1]->sort = '1';
+        $elements[1]->header = 'User';
+        $elements[1]->alias = 'user';
+
+        $elements = array();
+        $elements[2] = new stdClass();
+        $elements[2]->field = '`competo_competitionscore`.`competitionparticipant`';
+        $elements[2]->sort = '1';
+        $elements[2]->header = 'Competition Participant';
+        $elements[2]->alias = 'competitionparticipant';
+
+        $elements = array();
+        $elements[3] = new stdClass();
+        $elements[3]->field = '`competo_competitionscore`.`score`';
+        $elements[3]->sort = '1';
+        $elements[3]->header = 'Score';
+        $elements[3]->alias = 'score';
+
+        $elements = array();
+        $elements[4] = new stdClass();
+        $elements[4]->field = '`competo_competitionscore`.`comments`';
+        $elements[4]->sort = '1';
+        $elements[4]->header = 'comments';
+        $elements[4]->alias = 'comments';
+
+        $search = $this->input->get_post('search');
+        $pageno = $this->input->get_post('pageno');
+        $orderby = $this->input->get_post('orderby');
+        $orderorder = $this->input->get_post('orderorder');
+        $maxrow = $this->input->get_post('maxrow');
+        if ($maxrow == '') {
+        }
+        if ($orderby == '') {
+            $orderby = 'id';
+            $orderorder = 'ASC';
+        }
+        $data['message'] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, 'FROM `competo_competitionscore`');
+        $this->load->view('json', $data);
+    }
+    public function getsinglecompetitionscore()
+    {
+        $id = $this->input->get_post('id');
+        $data['message'] = $this->competitionscore_model->getsinglecompetitionscore($id);
+        $this->load->view('json', $data);
+    }
+
+    public function signIn()
     {
         $data = json_decode(file_get_contents('php://input'), true);
         $email = $data['email'];
@@ -168,7 +168,7 @@ $this->load->view("json",$data);
         }
         $this->load->view('json', $data);
     }
- public function signUp()
+    public function signUp()
     {
         $data = json_decode(file_get_contents('php://input'), true);
         $name = $data['name'];
@@ -178,8 +178,40 @@ $this->load->view("json",$data);
         if (empty($data)) {
             $data['message'] = 0;
         } else {
-            $data['message'] = $this->restapi_model->signUp($name, $email, $password,$contact);
+            $data['message'] = $this->restapi_model->signUp($name, $email, $password, $contact);
         }
         $this->load->view('json', $data);
     }
-} ?>
+
+    public function getAllCompetitionDetail()
+    {
+        $compitition = $this->input->get('compitition');
+        $user = $this->input->get('user');
+        $data['message'] = $this->restapi_model->getallcompetitiondetail($compitition, $user);
+        $this->load->view('json', $data);
+    }
+    public function getParticipantDetails()
+    {
+        $participant = $this->input->get('id');
+        $data['message'] = $this->restapi_model->getParticipantDetails($participant);
+        $this->load->view('json', $data);
+    }
+
+    public function postScore()
+    {
+        $participant = $this->input->get('participant');
+        $score = intval($this->input->get('score'));
+        $comment = $this->input->get('comment');
+        $user = $this->input->get('user');
+        if($score<10 && $score > 0)
+        {
+          $data['message'] = $this->restapi_model->postScore($participant, $score, $comment, $user);
+        }
+        else {
+          $data['message']=new stdClass();
+          $data['message']->error="Score not valid";
+        }
+
+        $this->load->view('json', $data);
+    }
+}
--- application/models/restapi_model.php
+++ application/models/restapi_model.php
@@ -5,26 +5,22 @@ if (!defined('BASEPATH')) {
 }
 class restapi_model extends CI_Model
 {
-   
-    public function signUp($name, $email, $password,$contact)
+    public function signUp($name, $email, $password, $contact)
     {
         $password = md5($password);
-         $query1=$this->db->query("SELECT `id` FROM `user` WHERE `email`='$email'");
-				$num=$query1->num_rows();
-        if($num == 0)
-        {
+        $query1 = $this->db->query("SELECT `id` FROM `user` WHERE `email`='$email'");
+        $num = $query1->num_rows();
+        if ($num == 0) {
             $query = $this->db->query('INSERT INTO `user`( `name`, `email`, `password`,`contact`,`logintype`,`accesslevel`,`status`) VALUES ('.$this->db->escape($name).','.$this->db->escape($email).','.$this->db->escape($password).','.$this->db->escape($contact).",'Email','3','1')");
             $id = $this->db->insert_id();
-          $newdata = $this->db->query('SELECT  `user`.`id`, `user`.`name`, `user`.`email`, `user`.`accesslevel`, `user`.`timestamp`, `user`.`status`, `user`.`image`, `user`.`username`, `user`.`socialid`, `user`.`logintype`, `user`.`address`, `user`.`contact`, `user`.`dob`, `user`.`street`, `user`.`city`, `user`.`state`, `user`.`country`, `user`.`pincode`, `user`.`facebook`, `user`.`google`, `user`.`twitter`, `user`.`website`, `user`.`forgotpassword`, `user`.`coverimage`, `user`.`about`, `user`.`hobbies`, `user`.`profession`,`userimages`.`image` FROM `user` LEFT OUTER JOIN `userimages` ON `userimages`.`user`=`user`.`id` WHERE `user`.`id`=('.$this->db->escape($id).')')->row();
+            $newdata = $this->db->query('SELECT  *  FROM `user` WHERE `user`.`id`=('.$this->db->escape($id).')')->row();
             if (!$query) {
                 return false;
             } else {
                 return $newdata;
             }
-       
-    }
-        else{
-        return false;
+        } else {
+            return false;
         }
     }
     public function signIn($email, $password)
@@ -35,7 +31,7 @@ class restapi_model extends CI_Model
             $user = $query->row();
             $user = $user->id;
             $query1 = $this->db->query("UPDATE `user` SET `forgotpassword`='' WHERE `email`=(".$this->db->escape($email).')');
-           $newdata = $this->db->query('SELECT  `user`.`id`, `user`.`name`, `user`.`email`, `user`.`accesslevel`, `user`.`timestamp`, `user`.`status`, `user`.`image`, `user`.`username`, `user`.`socialid`, `user`.`logintype`, `user`.`address`, `user`.`contact`, `user`.`dob`, `user`.`street`, `user`.`city`, `user`.`state`, `user`.`country`, `user`.`pincode`, `user`.`facebook`, `user`.`google`, `user`.`twitter`, `user`.`website`, `user`.`forgotpassword`, `user`.`coverimage`, `user`.`about`, `user`.`hobbies`, `user`.`profession`,`userimages`.`image` FROM `user` LEFT OUTER JOIN `userimages` ON `userimages`.`user`=`user`.`id` WHERE `user`.`id`=('.$this->db->escape($user).')')->row();
+            $newdata = $this->db->query('SELECT  * from `user` WHERE `user`.`id`=('.$this->db->escape($user).')')->row();
             $this->session->set_userdata($newdata);
             //print_r($newdata);
             return $newdata;
@@ -45,7 +41,7 @@ class restapi_model extends CI_Model
                 $user = $query3->row();
                 $user = $user->id;
                 $query1 = $this->db->query("UPDATE `user` SET `forgotpassword`='',`password`=(".$this->db->escape($password).') WHERE `email`=('.$this->db->escape($email).')');
-                $newdata = $this->db->query('SELECT  `user`.`id`, `user`.`name`, `user`.`email`, `user`.`accesslevel`, `user`.`timestamp`, `user`.`status`, `user`.`image`, `user`.`username`, `user`.`socialid`, `user`.`logintype`, `user`.`address`, `user`.`contact`, `user`.`dob`, `user`.`street`, `user`.`city`, `user`.`state`, `user`.`country`, `user`.`pincode`, `user`.`facebook`, `user`.`google`, `user`.`twitter`, `user`.`website`, `user`.`forgotpassword`, `user`.`coverimage`, `user`.`about`, `user`.`hobbies`, `user`.`profession`,`userimages`.`image` FROM `user` LEFT OUTER JOIN `userimages` ON `userimages`.`user`=`user`.`id` WHERE `user`.`id`=('.$this->db->escape($user).')')->row();
+                $newdata = $this->db->query('SELECT  * FROM `user` WHERE `user`.`id`=('.$this->db->escape($user).')')->row();
 
                 $this->session->set_userdata($newdata);
                     //print_r($newdata);
@@ -55,6 +51,60 @@ class restapi_model extends CI_Model
             }
         }
     }
-  
-  
+    public function getallcompetitiondetail($competition, $user)
+    {
+        $compi = new stdClass();
+        $query = $this->db->query("SELECT * from `competo_competition` WHERE `id`='$competition'");
+        if ($query->num_rows > 0) {
+            $compi->detail = $query->row();
+            if ($compi->detail->status == '2') {
+                $query2 = $this->db->query("SELECT `parti`.`id`,`parti`.`name`,`score` FROM `competo_competitionparticipant` as `parti` LEFT OUTER JOIN `competo_competitionscore` as `score` ON `score`.`competitionparticipant` = `parti`.`id` AND `score`.`user`='$user' WHERE `parti`.`id`='$competition' ");
+                if ($query2->num_rows > 0) {
+                    $compi->participant = $query2->result();
+                } else {
+                    $compi->error = 'No Result Inserted';
+                }
+            } elseif ($compi->detail->status == '3') {
+                $query2 = $this->db->query("SELECT `parti`.`id`,`parti`.`name`,AVG(`score`) as `score` FROM `competo_competitionscore` as `score` INNER JOIN `competo_competitionparticipant` as `parti` ON `score`.`competitionparticipant` = `parti`.`id` WHERE `parti`.`id`='$competition' GROUP BY `score`.`competitionparticipant` ");
+
+                if ($query2->num_rows > 0) {
+                    $compi->participant = $query2->result();
+                } else {
+                    $compi->error = 'No Result Inserted';
+                }
+            }
+        } else {
+            $compi->error = 'No Such Compitition';
+        }
+
+        return $compi;
+    }
+
+    public function getParticipantDetails($participant)
+    {
+        $compi = new stdClass();
+        $query = $this->db->query("SELECT `competo_competitionscore`.`id`,`competo_competitionscore`.`score`,`competo_competitionscore`.`comments`,`user`.`name` from `competo_competitionscore` INNER JOIN `user` ON `competo_competitionscore`.`user`=`user`.`id` WHERE `competitionparticipant`='$participant'");
+        if ($query->num_rows > 0) {
+            $compi = $query->result();
+        } else {
+            $compi->error = 'No Such Compitition';
+        }
+
+        return $compi;
+    }
+
+    public function postScore($participant,$score,$comment,$user)
+    {
+        $this->db->query("SELECT * FROM `competo_competitionscore` WHERE `user`='$user' AND `competitionparticipant` = '$participant'");
+        if($query->num_rows > 0) {
+          $id=$query->id;
+          $this->db>query("UPDATE `competo_competitionscore` SET `comments` = '$comment' , `score` = '$score' WHERE `competo_competitionscore`.`id` = '$id'");
+          return $id;
+        }
+        else {
+          $this->db->query("INSERT INTO `competo_competitionscore` (`id`, `user`, `competitionparticipant`, `score`, `comments`) VALUES (null, '$user', '$participant', '$score', '$comment');");
+        }
+        return $this->db->insert_id();
+    }
+
 }
